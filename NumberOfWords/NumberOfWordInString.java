1041e5dc152a2be86d0ca14edb2e43fb476f6c98
Vladimir Chantitch <chantitch.vladimir@gmail.com>
2023-04-12 15:04:23 +0200

    Fix the regex and the file duplication

--- NumberOfWords/NumberOfWordInString.java
+++ NumberOfWords/NumberOfWordInString.java
@@ -23,19 +23,19 @@ public class NumberOfWordInString {
     }
 
     public static void PrintNumberOfWords(String txt){
-        Integer numberOfWords = txt.split(" ").length;
+        Integer numberOfWords = txt.trim().split("\\b\\w+\\b").length;
         System.out.println(new StringBuilder("The number of words in your sentence is :: ").append(numberOfWords));
     }
 
     public static void WriteStringInFile(String txt){
         try{
             File newFile = new File("aFile.txt");
+            newFile.delete();
             if (newFile.createNewFile()){
                 System.out.println("Well well well you created a new file : " + newFile.getName());
                 FileWriter fw = new FileWriter(newFile, true);
                 BufferedWriter bw = new BufferedWriter(fw);
                 bw.write(txt);
-
 
                 bw.close();
                 fw.close();
