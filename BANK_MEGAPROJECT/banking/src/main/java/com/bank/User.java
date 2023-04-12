0b9a158bd808896536a0ba0ce41a231c183d303c
Vladimir Chantitch <chantitch.vladimir@gmail.com>
2023-04-12 16:55:59 +0200

    Init

---
+++ BANK_MEGAPROJECT/banking/src/main/java/com/bank/User.java
@@ -0,0 +1,7 @@
+package Java_Projects.BANK_MEGAPROJECT.banking.src.main.java.com.bank;
+
+public class User {
+    public String firstName;
+    public String lastName;
+    public String accountID;
+}
