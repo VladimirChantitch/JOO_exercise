5bc88897d44b5c13baa2dd05c3a426443670bc2a
Vladimir Chantitch <chantitch.vladimir@gmail.com>
2023-04-13 09:57:13 +0200

    Singleton ok

--- BANK_MEGAPROJECT/banking/src/main/java/com/bank/App.java
+++ BANK_MEGAPROJECT/banking/src/main/java/com/bank/App.java
@@ -13,7 +13,6 @@ public class App
     static boolean isQuitting = false;
     public static void main( String[] args )
     {
-        new DbHelper();
         bankService = new BankService();
         ControlPanelHandler();
     }
@@ -52,19 +51,19 @@ public class App
     private static void HandleInput(Integer controlInput){
         switch (controlInput) {
             case 1:
-                System.out.println("1");
+
                 break;
             case 2:
-                System.out.println("2");
+
                 break;
             case 3:
-                System.out.println("3");
+
                 break;
             case 4:
-                System.out.println("4");
+
                 break;
             case 5:
-                System.out.println("5");
+
                 break;
         }
     }
--- BANK_MEGAPROJECT/banking/src/main/java/com/bank/Servives/DbHelper.java
+++ BANK_MEGAPROJECT/banking/src/main/java/com/bank/Servives/DbHelper.java
@@ -3,7 +3,14 @@ package Java_Projects.BANK_MEGAPROJECT.banking.src.main.java.com.bank.Servives;
 public class DbHelper {
     public static DbHelper Instance;
 
-    public DbHelper(){
+    private DbHelper(){
         Instance = this;
+    }
+
+    public static DbHelper getInstance(){
+        if (Instance == null){
+            new DbHelper();
+        }
+        return Instance;
     }
 }
